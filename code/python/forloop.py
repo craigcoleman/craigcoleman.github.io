045f2ba610266b1ce9a8aa6a9d9ab62005f34744
craigcoleman <coleman@owt.com>
2015-11-09 04:38:26 -0800

    all

--- code/python/forloop.py
+++ code/python/forloop.py
@@ -1,8 +1,8 @@
 x = raw_input("enter the password: ");
 y = "";
 for c in x:
-	y += chr(ord(c) ^ 14);
-	print(y)
+	print(x);
+	print(c);
 	if y == "ko}wmzhugQocQoQhbois":
 		print "congratz the flag is " + y;
 	else:
